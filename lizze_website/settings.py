6cf9a82d70da2ae3b1c53c8fc3d6b5d541db4665
cashycrown <olamideadedokun36@gmail.com>
2025-08-02 05:16:19 -0700

    Fix: Installed python-decouple

--- lizze_website/settings.py
+++ lizze_website/settings.py
@@ -14,10 +14,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config(
     'ALLOWED_HOSTS',
